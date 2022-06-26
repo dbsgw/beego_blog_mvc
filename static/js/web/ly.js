89165320511deb14f2418b1a82d0962c8e52c3d9
dbsgw <admin@dbsgw.cn>
2022-06-26 21:30:27 +0800

    推荐随机颜色

--- static/js/web/ly.js
+++ static/js/web/ly.js
@@ -1,3 +1,18 @@
-// $('.acrid').find(".acrid_item1:nth-child(1)").find('a').css({"color": "red"})
-// $('.acrid').find(".acrid_item1:nth-child(2)").css({"color": "red"})
-// $('.acrid').find(".acrid_item1:nth-child(3)").css({"color": "red"})
+class Home{
+    inits(){
+        this.acrid()
+    }
+    // 热门推荐随机颜色
+    acrid(){
+        const colors = ["#FE2D46", "#F60000", "#FAA90E"]
+        let acids = $('.acrid').find(".acrid_label")
+        for (let i = 0; i < 5; i++) {
+            if (i < 3) {
+                $(acids[i]).css({"backgroundColor": colors[i]})
+            }
+        }
+    }
+}
+
+let home = new Home
+home.inits()
